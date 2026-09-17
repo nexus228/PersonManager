46ae52fc15b12ec56a38ceec9a51932dd5016c82
Testpilot228 <denny.sikora@gmx.de>
2026-09-17 09:47:33 +0200

    Projektdateien hinzufügen.

---
+++ PersonManager_WPF_CLIENT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PersonManager_WPF_CLIENT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
